be0f0f8dd03b0dd5eb11ae8083f9633f46d6be53
Kacper Augustyniak <Yosemite@macbook-pro-kacper.local>
2016-06-05 09:32:43 +0200

    Initial Commit

---
+++ Qpony_Zadanie/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Qpony_Zadanie
+//
+//  Created by Kacper Augustyniak on 05/06/16.
+//  Copyright © 2016 Kacper Augustyniak. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
